36eaa8c01cd86f9acba876cc214ab7b4eec1a145
Joao Bornelli <joao.bornelli@edu.sc.senai.br>
2025-07-18 19:22:36 -0300

    Refactor robot control logic and enhance safety features

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+from enum import Enum
 import math
 from coppeliasim_zmqremoteapi_client import *
 import time
+
+import _thread
+
+
+
 
 client = RemoteAPIClient()
 sim = client.require('sim')
@@ -13,17 +19,29 @@ robot_name = "PioneerP3DX"
 
 #Constants
 
-BACKUP_DISTANCE = 0.3
 LAST_TURN = "left"
 DEVIATION_LIMIT = 0.01
+RETURN_REVERSE_DIST = 0.2
+DEVIATE_REVERSE_DIST = 0.1
 
+SAFETY_ACT = False
 
-def backupALitte():
+class Side(Enum):
+    LEFT = 1
+    RIGHT = 0
+
+def dontKYS():
+    print("VAI CAIRRRR!!!")
+    motorControl(0.0,0.0)
+    backupALitte(RETURN_REVERSE_DIST)
+    turnBack()
+
+def backupALitte(reverse_distance):
     print("Dando ré")
     dist_total = 0
     motorControl(-0.5,-0.5)
     starTime = sim.getSimulationTime()
-    while(abs(dist_total) < BACKUP_DISTANCE):
+    while(abs(dist_total) < reverse_distance):
         linearVelocity, _ = sim.getObjectVelocity(gyro_handle)
         delta = sim.getSimulationTime() - starTime  
         dist_total =+ linearVelocity[0] * delta
@@ -34,14 +52,13 @@ def motorControl(left,right):
     sim.setJointTargetVelocity(left_motor_handle,left)
     sim.setJointTargetVelocity(right_motor_handle,right)
 
-def turn180(side):
+def turn(side,angle):
     print("Virando")
     degree = 0
-    backupALitte()
     match side:
-        case "right":
+        case side.RIGHT:
             motorControl(0.4,-0.05)
-        case "left":
+        case side.LEFT:
             motorControl(-0.05,0.4)
             
         case _:
@@ -49,12 +66,12 @@ def turn180(side):
             return 0
         
     currTime = lastTime = sim.getSimulationTime()
-    while( degree < 180):
+    while( degree < angle):
         currTime = sim.getSimulationTime()
         dt = currTime - lastTime
         lastTime = currTime
         degree += abs(angleDegree(dt))
-        print(f"Virou: {degree:.3f}")
+        #print(f"Virou: {degree:.3f}")
     motorControl(0.0,0.0)
     
     
@@ -64,7 +81,20 @@ def angleDegree(delta):
     angulo_total_rad = angularVelocity[2] * delta
     return math.degrees(angulo_total_rad)
     
-    
+def deviate(side):
+    print(side)
+    match side:
+        case side.LEFT:
+            motorControl(0.0,0.0)
+            backupALitte(DEVIATE_REVERSE_DIST)
+            turn(side.LEFT,90)
+            turn(side.RIGHT,90)
+        case side.RIGHT:
+            motorControl(0.0,0.0)
+            backupALitte(DEVIATE_REVERSE_DIST)
+            turn(side.RIGHT,90)
+            turn(side.LEFT,90)
+               
     
 sim.startSimulation()
 try:
@@ -76,21 +106,38 @@ try:
     IRFront_handle = sim.getObject(f'/{robot_name}/IRFront')
     
     bumperFront_handle = sim.getObject(f'/{robot_name}/bumperFront')
-    bumperLeft_handle = sim.getObject(f'/{robot_name}/bumperLeft')
+    bumperLeft_handle  = sim.getObject(f'/{robot_name}/bumperLeft')
     bumperRight_handle = sim.getObject(f'/{robot_name}/bumperRight')
+    
+    frontDrop_handle = sim.getObject(f'/{robot_name}/dropFront')
+    rightDrop_handle = sim.getObject(f'/{robot_name}/dropRight')
+    leftDrop_handle  = sim.getObject(f'/{robot_name}/dropLeft')
     
     
     print("Todos os objetos encontrados")
+    safety = _thread.start_new_thread(dontKYS,())
     
 except Exception as e:
     print(f"Erro ao obter os handles: {e}")
     sim.stopSimulation()
     exit()
     
-
+def turnBack():
+    global LAST_TURN
+    if LAST_TURN == Side.RIGHT:
+        backupALitte(RETURN_REVERSE_DIST)
+        turn(Side.LEFT,180)
+        LAST_TURN = Side.LEFT
+    else:
+        backupALitte(RETURN_REVERSE_DIST)
+        turn(Side.RIGHT,180)
+        LAST_TURN = Side.RIGHT
+    
     
 currTime = lastTime = sim.getSimulationTime()
-angleDeviation = 0  # Initialize angleDeviation before the loop
+angleDeviation = 0
+LAST_TURN = Side.LEFT
+
 while sim.getSimulationState() != sim.simulation_stopped:
     if angleDeviation > DEVIATION_LIMIT:
         motorControl(0.8+angleDeviation,0.8)
@@ -104,20 +151,35 @@ while sim.getSimulationState() != sim.simulation_stopped:
     dt = currTime - lastTime
     lastTime = currTime
     degree = angleDegree(dt)
-    result_bumper_frente, _, _, _, _ = sim.readProximitySensor(bumperFront_handle)
+    result_bumper_front, bumper_front_dist, _, _, _ = sim.readProximitySensor(bumperFront_handle)
+    result_bumper_right, bumper_right_dist, _, _, _ = sim.readProximitySensor(bumperRight_handle)
+    result_bumper_left,  bumper_left_dist,  _, _, _ = sim.readProximitySensor(bumperLeft_handle)
 
-    angleDeviation += degree * dt * 0.5 #K integral
+    
+    
+    angleDeviation += degree * dt * 0.6 #K integral
     print(f"integral: {angleDeviation}")
     
-    if(result_bumper_frente == 1):
-        
+    #print(f'SR: {bumper_right_dist:.2f}/ SF: {bumper_front_dist:.2f}/ SL: {bumper_left_dist:.2f}/')
+    if SAFETY_ACT:
+        print('Detectada queda!')
+        turnBack()
+    
+    if result_bumper_front:     
         print("Tocou!")
-        if LAST_TURN == "right":
-            turn180("left")
-            LAST_TURN = "left"
-        else:
-            turn180("right")
-            LAST_TURN = "right"
+        turnBack()
+        angleDeviation = 0
+        lastTime = sim.getSimulationTime()
+        
+    if result_bumper_left:
+        print("Tocou LEFT!")
+        deviate(Side.RIGHT)
+        angleDeviation = 0
+        lastTime = sim.getSimulationTime()
+    
+    if result_bumper_right:
+        print("Tocou! RIGHT")
+        deviate(Side.LEFT)
         angleDeviation = 0
         lastTime = sim.getSimulationTime()
         
@@ -125,4 +187,3 @@ while sim.getSimulationState() != sim.simulation_stopped:
  
 print("Teste finalizado")
 sim.stopSimulation()
-
